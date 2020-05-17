87546e92e927d768afa9f6442a5a71ca4dabe3c9
zhanghua <1582373436@qq.com>
2020-05-17 19:51:23 +0800

    Login组件提交

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
+import './plugins/element.js'
+import router from './router'
+import './assets/css/global.css'
+import axios from 'axios'
+
+axios.defaults.baseURL = 'https://www.liulongbin.top:8888/api/private/v1/'
+Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  router
 }).$mount('#app')
